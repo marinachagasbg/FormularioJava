ef0c45373b83ea88e6b10d6db9b68da8d2d8acd7
Hguigit <guilhermesps2@gmail.com>
2021-07-31 15:30:07 -0300

    Adding backend and DB connection

---
+++ Tarefa/src/backend/ControllerCidadeEstado.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+
+/**
+ *
+ * @author guilherme
+ */
+public class ControllerCidadeEstado {
+    
+    static ConnectionDB database = new ConnectionDB();
+    
+    
+    public static boolean InsereEstado(String estado, String sigla){
+        boolean retorno;
+		
+        String sql = "INSERT INTO estados(estado_nome, estado_sigla) VALUES (" + estado + "," + sigla + ")";
+        retorno = database.ExecutaSQL(sql);
+        
+        return retorno;
+    }
+    public static boolean InsereCidade(String cidade){
+        boolean retorno;
+		
+        String sql = "INSERT INTO cidades(cidade_nome) VALUES (" + cidade + ")";
+        retorno = database.ExecutaSQL(sql);
+      
+        return retorno;
+    }
+    
+}
